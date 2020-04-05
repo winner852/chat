bbe2956fe8af39c0e855d983f681f3dda154ffb1
winner852 <winnersouza852@gmil.com>
2020-04-05 13:25:53 -0300

    adicionando projeto ao github

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Chat Flutter
 
 Chat feito em Flutter integrado ao Firebase
+
+Requer Flutter v1.12.13+hotfix.5-stable ou versão acima
+
+
+Inicie um novo projeto no Firebase e crie um app android
+
+Lembre-se de configurar as permissões do seu banco de dados
+
+adicione o arquivo google-services.json dentro da pasta:
+
+chat\android\app
+
+lembre-se de editar o arquivo build.gradle adicionando o id da sua aplicação
+
+chat\android\app\build.gradle
+
+	applicationId "br.com.win32"
+
+App desenvolvido durante o curso de Flutter da Udemy
+Professor Daniel Ciolfi
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        ),
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
